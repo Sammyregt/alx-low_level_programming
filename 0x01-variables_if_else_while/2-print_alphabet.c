0fda9e9b1dceb8e3154f3156765777d7f88f8ad7
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-01 14:27:24 -0700

    print alhabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int x;
 
-	for (x = 'A'; x <= 'Z'; x++)
+	for (x = 0; x < 26; x++)
 	{
-		putchar(tolower(x));
+		putchar(alp[x]);
 	}
+	putchar('\n');
 	return (0);
 }
