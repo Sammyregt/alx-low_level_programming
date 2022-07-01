2ccd5e3a8f6a7d1f61de49ac731ba74089d88504
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-01 15:19:22 -0700

    print numbers using char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - print numbers between 0 to 9
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
