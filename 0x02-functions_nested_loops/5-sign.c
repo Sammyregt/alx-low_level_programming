4ad20495019684d8b99a2c659e191043594322d7
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-05 14:18:12 -0700

    print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Determine if a number is greater than zero, is zero or less than zero
+ *
+ * @n: the character in ASII code
+ *
+ * Return: 1 if n greater than zero. 0 if n is zero and -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
