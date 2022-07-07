8fce5faa13685be082b29d3f497f9a69097b4951
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-07 13:57:55 -0700

    print number from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - display number from 0 - 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
