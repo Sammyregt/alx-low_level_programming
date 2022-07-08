becf9da750476788ea3b5ba3fad2992e79795540
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-08 04:38:04 -0700

    print diagonal line diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines n times
+ *
+ * @n: times diagonal lines are printed
+ *
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int p, q;
+
+	for (p = 0; p < n; p++)
+	{
+		for (q = 0; q < p; q++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (p < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sqaure - print hashes squares
+ *
+ * @size: size of the square
+ *
+ * Return: no return
+ */
+
+void print_square(int size)
+{
+	int p, q;
+
+	for (p = 0; 
