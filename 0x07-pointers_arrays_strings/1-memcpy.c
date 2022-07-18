890a31d1ce73554944af37bb18f1312ad7751123
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-18 13:22:38 -0700

    copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _memcpy - copies memory area,
+ *
  * @dest: destination memory area.
  * @src: source memory area.
  * @n: bytes filled.
+ *
  * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
