7290cc18d21b3627730b9fa113ebe429a39c74df
Sammyregt <adeoyesamuel100@gmail.com>
2022-07-26 17:25:08 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed bt a new line
+ * 
+ * @argc: number of command line argments
+ * @argv: array that contains the program command line arguments
+ *
+ * Return: 0 - success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Projects done during my full stack software engineering studies at alx
