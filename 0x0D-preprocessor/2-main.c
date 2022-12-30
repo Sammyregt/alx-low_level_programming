413f52d2ef58a31c4188b455fae35b04db57f761
Sammyregt <adeoyesamuel100@gmail.com>
2022-12-30 16:19:56 +0000

    Print name of file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/* main - return the name of the name of file
- * Return - Always 0
+/**
+ * main - return the name of the name of file
+ *
+ * Return: Always 0
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return(0);
+	return (0);
 }
